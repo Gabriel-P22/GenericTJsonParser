a068da60c010c6b3239d6bf14c53399d60617fb1
gpaschoal <Gabriel.Paschoal-ext@ab-Inbev.com>
2023-03-09 22:42:19 -0300

    Finished json file parser

---
+++ decodeJsonFiles/Extensions/JsonFiles.swift
@@ -0,0 +1,8 @@
+//
+//  JsonFiles.swift
+//  decodeJsonFiles
+//
+//  Created by Gabriel Paschoal on 09/03/23.
+//
+
+import Foundation
---
+++ decodeJsonFiles/Parser/ParserFile.swift
@@ -0,0 +1,8 @@
+//
+//  ParserFile.swift
+//  decodeJsonFiles
+//
+//  Created by Gabriel Paschoal on 09/03/23.
+//
+
+import Foundation
