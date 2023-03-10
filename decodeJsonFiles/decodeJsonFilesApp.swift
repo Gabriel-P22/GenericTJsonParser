79cac66de55abc890b088780d4df4b362d9c14cf
gpaschoal <Gabriel.Paschoal-ext@ab-Inbev.com>
2023-03-09 22:00:46 -0300

    Initial Commit

---
+++ decodeJsonFiles/decodeJsonFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  decodeJsonFilesApp.swift
+//  decodeJsonFiles
+//
+//  Created by Gabriel Paschoal on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct decodeJsonFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
